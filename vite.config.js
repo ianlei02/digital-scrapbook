01edbc6fef70dd1cd0e0d02cbeb4cb0c288c95e8
Ian Lei Castillo <ianleicastillo.basc@gmail.com>
2025-11-16 21:07:39 +0800

    Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/digital-scrapbook/',
+  base: '/',
+  // base: '/digital-scrapbook/',
   assetsInclude: ['**/*.glb'],
   resolve: {
     alias: {
